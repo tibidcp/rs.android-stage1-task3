f05eac060978f72c74058cf9d7f9ed79d2d6b894
Tibeikin Oleg <tibidcp@gmail.com>
2020-05-01 23:33:28 +0300

    add Polynomial

--- src/main/kotlin/subtask1/PolynomialConverter.kt
+++ src/main/kotlin/subtask1/PolynomialConverter.kt
@@ -1,9 +1,23 @@
 package subtask1
 
-class PolynomialConverter {
+import kotlin.math.absoluteValue
 
-    // TODO: Complete the following function
+class PolynomialConverter {
     fun convertToStringFrom(numbers: Array<Int>): String? {
-        throw NotImplementedError("Not implemented")
+        if (numbers.isEmpty()) {
+            return null
+        }
+        return numbers.foldIndexed("") { index, string, number ->
+            if (number == 0)
+                return@foldIndexed string
+            val stringNumber = if (number.absoluteValue == 1) "" else number.absoluteValue
+            val size = numbers.size
+            val delimiter = if (index == 0) "" else {if (number < 0) " - " else " + "}
+            string + when (index) {
+                size - 1 -> delimiter + stringNumber
+                size - 2 -> "$delimiter${stringNumber}x"
+                else -> "$delimiter${stringNumber}x^${size - index - 1}"
+            }
+        }
     }
 }
