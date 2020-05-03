583fe45cc126f7beb9fcfd77c167a90f48538954
Tibeikin Oleg <tibidcp@gmail.com>
2020-05-03 15:01:49 +0300

    add Combinator

--- src/main/kotlin/subtask2/Combinator.kt
+++ src/main/kotlin/subtask2/Combinator.kt
@@ -1,9 +1,18 @@
 package subtask2
 
 class Combinator {
-
-    // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        val m = array[0]
+        val n = array[1]
+        for (k in 1 until n) {
+            if (combinationCount(n, k).toInt() == m)
+                return k
+        }
+        return null
     }
+
+    private fun combinationCount(n: Int, k: Int) =
+        factorial(n) / (factorial(n - k) * factorial(k))
+
+    private fun factorial(num: Int) = (1..num.toLong()).reduce(Long::times)
 }
