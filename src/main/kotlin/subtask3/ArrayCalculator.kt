ea36542459c3c6977c22366d265ec69175781736
Tibeikin Oleg <tibidcp@gmail.com>
2020-05-03 21:44:03 +0300

    add ArrayCalculator

--- src/main/kotlin/subtask3/ArrayCalculator.kt
+++ src/main/kotlin/subtask3/ArrayCalculator.kt
@@ -1,9 +1,39 @@
 package subtask3
 
-class ArrayCalculator {
+import kotlin.math.absoluteValue
 
-    // TODO: Complete the following function
+class ArrayCalculator {
     fun maxProductOf(numberOfItems: Int, itemsFromArray: Array<Any>): Int {
-        throw NotImplementedError("Not implemented")
+        val list = itemsFromArray.filterIsInstance<Int>().sortedByDescending { it.absoluteValue }
+        if (list.isEmpty()) {
+            return 0
+        }
+        if (list.size <= numberOfItems) {
+            return list.reduce(Int::times)
+        }
+        return comb(list.size, numberOfItems)
+            .map { indexList -> list.filterIndexed { index, _ -> index in indexList }
+                .reduce(Int::times) }.max()!!
+    }
+
+    private fun comb(n: Int, k: Int) : List<List<Int>> {
+        val listOfComb = mutableListOf<List<Int>>()
+        val list = (0 until k).toMutableList()
+        listOfComb.add(list.toList())
+        while (true) {
+            var i = k - 1
+            while (i >= 0 && list[i] + k - i + 1 > n) {
+                i--
+            }
+            if (i < 0) {
+                break
+            }
+            list[i] += 1
+            for (j in (i + 1) until k) {
+                list[j] = list[j - 1] + 1
+            }
+            listOfComb.add(list.toList())
+        }
+        return listOfComb
     }
 }
