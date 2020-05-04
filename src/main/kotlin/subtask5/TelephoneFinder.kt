e03822524ada4b21174337d7bf3b00ea0a0dab85
Tibeikin Oleg <tibidcp@gmail.com>
2020-05-04 14:18:06 +0300

    add TelephoneFinder

--- src/main/kotlin/subtask5/TelephoneFinder.kt
+++ src/main/kotlin/subtask5/TelephoneFinder.kt
@@ -1,11 +1,30 @@
 package subtask5
 
-import kotlin.reflect.KClass
-
 class TelephoneFinder {
-
-    // TODO: Complete the following function
     fun findAllNumbersFromGivenNumber(number: String): Array<String>? {
-        throw NotImplementedError("Not implemented")
+        if (number.contains('-')) {
+            return null
+        }
+        val numbers = mutableListOf<String>()
+        val neighbors = mapOf(
+            '1' to listOf('2', '4'),
+            '2' to listOf('1', '5', '3'),
+            '3' to listOf('2', '6'),
+            '4' to listOf('1', '5', '7'),
+            '5' to listOf('2', '4', '6', '8'),
+            '6' to listOf('3', '5', '9'),
+            '7' to listOf('4', '8'),
+            '8' to listOf('0', '5', '7', '9'),
+            '9' to listOf('6', '8'),
+            '0' to listOf('8'))
+
+        number.forEachIndexed {index, char ->
+            neighbors[char]?.forEach {neighbor ->
+                val numList = number.toMutableList()
+                numList[index] = neighbor
+                numbers.add(numList.joinToString(""))
+            }
+        }
+        return numbers.toTypedArray()
     }
 }
